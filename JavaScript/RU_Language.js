8fcb724409066792cbe84c058902acf0ca22ef5e
Rodion Gimranov <80162183+RodionGimranov@users.noreply.github.com>
2023-09-15 19:47:52 +0600

    Add files via upload

---
+++ JavaScript/RU_Language.js
@@ -0,0 +1,91 @@
+// Language translation
+const LanguageVersions = {
+    en: {
+        // Header
+        "lang-home_btn": "Home",
+        "lang-project_btn": "Project",
+        "lang-contact_btn": "Contacts",
+
+        // Section "Home"
+        "lang-greetings_text1": "Hi, I'm Rodion",
+        "lang-greetings_text2": "Frontend Developer",
+        "lang-download_CV_btn": "Download CV",
+
+        // Section "About Me"
+        "lang-about_me_name_page": "About Me",
+        "lang-text_about_me1": "I am a beginner frontend developer. I have been studying at the KFU Institute of Computational Mathematics and Information Technology for the past three years. My specialization lies in creating multi-page websites and crafting simple yet elegant designs.",
+        "lang-text_about_me2": "On my website, you can explore my portfolio showcasing my projects and the tools I utilize. If you have any questions or would like to collaborate with me, feel free to reach out. I will be happy to answer all your inquiries!",
+        "lang-contact_me_btn": "Contact Me",
+
+        // Section "My Projects"
+        "lang-my_project_name_page": "My Projects",
+
+        "lang-background_project1": "In developing",
+        "lang-name_of_project1": "ToDo Web App",
+        "lang-more_info_btn1": "More info",
+
+        "lang-background_project2": "In developing",
+        "lang-name_of_project2": "Design Studio website",
+        "lang-more_info_btn2": "More info",
+
+        "lang-background_project3": "In developing",
+        "lang-name_of_project3": "Zoo Shop website",
+        "lang-more_info_btn3": "More info",
+
+        // Section "My tools and skills"
+        "lang-my_tools_and_skills_name_page": "My tools and skills",
+        "lang-text_my_tools_and_skills1":  "I use a variety of web development tools.",
+        "lang-text_my_tools_and_skills2": "VS Code aids in writing clean code, Figma assists in creating project layouts, and ChatGPT enhances text readability and user interface design.",
+
+        // Section "My contacts"
+        "lang-text_contact_me": "If you'd like to stay connected and keep up with my latest updates, visit my profiles on social media.",
+
+        // Section "Visual Settings"
+        "lang-fo_fun_name_page": "Visual Settings",
+        "lang-text_fo_fun": "In this section, I have compiled essential details regarding the color palette and text sizing employed throughout the website."
+    },
+    ru: {
+        // Шапка сайта
+        "lang-home_btn": "Главная",
+        "lang-project_btn": "Проекты",
+        "lang-contact_btn": "Контакты",
+
+        // Секция "Домашная страница"
+        "lang-greetings_text1": "Привет, я Родион",
+        "lang-greetings_text2": "Frontend Разработчик",
+        "lang-download_CV_btn": "Скачать CV",
+
+        // Секция "Обо Мне"
+        "lang-about_me_name_page": "Обо Мне",
+        "lang-text_about_me1": "Я начинающий фронтенд-разработчик. Последние три года я учусь в ИВМиИТ КФУ. Моя специализация заключается в создании многостраничных веб-сайтов и разработке простых, но элегантных дизайнов.",
+        "lang-text_about_me2": "На сайте вы увидите мое портфолио, проекты и инструменты, которые я использую. Если у вас есть какие-либо вопросы, не стесняйтесь обращаться ко мне. Буду рад ответить на все ваши вопросы!",
+        "lang-contact_me_btn": "Контакты",
+
+        // Секция "Мои Проекты"
+        "lang-my_project_name_page": "Мои проекты",
+
+        "lang-background_project1": "В разработке",
+        "lang-name_of_project1": "Веб приложение ToDo",
+        "lang-more_info_btn1": "Подробнее",
+
+        "lang-background_project2": "В разработке",
+        "lang-name_of_project2": "Сайт Студии дизайна",
+        "lang-more_info_btn2": "Подробнее",
+
+        "lang-background_project3": "В разработке",
+        "lang-name_of_project3": "Сайт Зоо Магазина",
+        "lang-more_info_btn3": "Подробнее",
+
+        // Секция "Мои инструменты и навыки"
+        "lang-my_tools_and_skills_name_page": "Мои навыки",
+        "lang-text_my_tools_and_skills1": "Я использую разнообразные инструменты для веб-разработки.",
+        "lang-text_my_tools_and_skills2": "VS Code помогает в написании чистого кода, Figma помогает в создании макетов проекта, а ChatGPT повышает читаемость текста и дизайн пользовательского интерфейса.",
+
+        // Секция "Мои контакты"
+        "lang-text_contact_me": "Если вы хотите оставаться на связи и следить за моими последними обновлениями, посетите мои профили в социальных сетях.",
+
+        // Секция "Визуальные настройки"
+        "lang-fo_fun_name_page": "Визуальные настройки",
+        "lang-text_fo_fun": "В данном разделе я собрал информацию, об палитре цветов и размеров текста, используемых на всем сайте."
+    }
+};
